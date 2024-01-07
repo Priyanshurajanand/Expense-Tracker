c2ca4ee6edc0093a1dd99b37407d1af3f4384092
Priyanshu Raj Anand <panand@iiitmanipur.ac.in>
2024-01-07 22:21:44 +0530

    Documentation added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      const { data } = await axios.post("https://expense-tracker-lqgn.onrender.com/api/v1/users/login", values);
+      const { data } = await axios.post("/users/login", values);
       setLoading(false);
       message.success("login success");
       localStorage.setItem(
