dac217757170d6e530d055002001b245552d4ef2
Priyanshu Raj Anand <panand@iiitmanipur.ac.in>
2024-01-07 17:02:44 +0530

    Added my project

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-4 fixed-bottom">
+      <h6 className="text-center text-white">
+        &copy; priyanshurajanand 2024. All rights reserved
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
